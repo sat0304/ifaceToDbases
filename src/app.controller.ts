b8fe6eb62b832f0c202f601c20631cc51695d225
Сергей Токарeв <sat0304@yandex.ru>
2023-05-08 15:32:20 +0400

    The genres and name of movie arrived to database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,16 +6,83 @@ import RabbitMQClient from './rabbitMQ/client';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/movies')
+  async getMovies(){
+    let route = {"routingKey": "getMovies"};
+    return RabbitMQClient.produceMovie(route);
+  }
+
+  @Get('/movies/:kinopoiskId')
+  async getMoviesByKinopoiskId(@Param('kinopoiskId') kinopoiskId: any){
+    let routeById = {
+      "routingKey": "getMovie", "kinopoiskId": kinopoiskId};
+    return RabbitMQClient.produceMovie(routeById);
+  }
+
+  @Get('/details')
+  async getDetails(){
+    let route = {"routingKey": "getDetails"};
+    return RabbitMQClient.produceMovie(route);
+  }
+
+  @Get('/details/:name')
+  async getDetailByName(@Param('name') name: any){
+    let routeByDetail = {
+      "routingKey": "getDetail", "name": name};
+    return RabbitMQClient.produceMovie(routeByDetail);
+  }
+
   @Get('/genres')
   async getGenres(){
     let route = {"routingKey": "getGenres"};
     return RabbitMQClient.produceMovie(route);
   }
 
-  @Get('/genres/:id')
-  async getById(@Param('id') id: number){
-    let routeById = {"routingKey": "getGenre", "id": id};
+  @Get('/genres/:genreEng')
+  async getGenreByName(@Param('genreEng') genreEng: any){
+    let routeByGenre = {
+      "routingKey": "getGenre", "genreEng": genreEng};
+    return RabbitMQClient.produceMovie(routeByGenre);
+  }
+
+  @Get('/countries')
+  async getCountries(){
+    let route = {"routingKey": "getCountries"};
+    return RabbitMQClient.produceMovie(route);
+  }
+
+  @Get('/countries/:countryId')
+  async getCountryByName(@Param('countryId') countryId: number){
+    let routeByCountry = {
+      "routingKey": "getCountry", "countryId": countryId
+    };
+    return RabbitMQClient.produceMovie(routeByCountry);
+  }
+
+  @Get('/persons')
+  async getPersons(){
+    let route = {"routingKey": "getPersons"};
+    return RabbitMQClient.produceMovie(route);
+  }
+
+  @Get('/persons/:kinopoiskId')
+  async getPersonsByKinopoiskId(@Param('kinopoiskId') kinopoiskId: any){
+    let routeById = {
+      "routingKey": "getPerson", "kinopoiskId": kinopoiskId};
     return RabbitMQClient.produceMovie(routeById);
+  }
+
+  @Get('/professions')
+  async getProfessions(){
+    let route = {"routingKey": "getProfessions"};
+    return RabbitMQClient.produceMovie(route);
+  }
+
+  @Get('/professions/:profession')
+  async getProfessionByName(@Param('profession') profession: any){
+    let routeByProfession = {
+      "routingKey": "getProfession", "profession": profession};
+    return RabbitMQClient.produceMovie(routeByProfession);
   }
 
   @Post()
