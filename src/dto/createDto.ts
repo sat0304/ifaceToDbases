4e7083e88203a11198d0313153dc83d595c95239
Сергей Токарeв <sat0304@yandex.ru>
2023-05-17 21:19:09 +0400

    number of  directores was reduced

--- src/dto/createDto.ts
+++
@@ -1,6 +0,0 @@
-// DTO data transfer object -промежуточный макет данных для маршрутизации,
-// вынесен в отдельный файл для разделения кода на функциональные подмодули
-export class CreateDto {
-    readonly nameMovie: string;
-    readonly description: string;
-}
